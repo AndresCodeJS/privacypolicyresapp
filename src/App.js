674d8df8b99a56b59f86bc9f0b717d2a9d3ce2c4
Andres Gomez <andres2661991@gmail.com>
2023-01-27 20:42:10 -0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div>
+        <p className="Title">Privacy Policy</p>
+      </div>
+
+      <div>
+        <p className="Text">
+          This Privacy Policy applies to the mobile application "Tasty Factory"
+          we publish on the Google Play Store. This Privacy Policy concerns the
+          personal data we collect from our users and explains how we collect,
+          use, and disclose it.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="Subtitle">Data Collection:</p>
+        <p className="Text">
+          We will not collect any kind of personal data from the users who
+          download and/or use the Application. We will only collect general
+          information about the use of the Application, such as the number of
+          users who download it, the amount of times it is used, etc., to
+          improve our services.
+        </p>
+        <p className="Subtitle">Data Use:</p>
+        <p className="Text">
+          The general information collected will not be used to identify any
+          particular user and will not be shared with anyone else. It will be
+          used solely to improve our services and the user experience.
+        </p>
+        <p className="Subtitle">Data Disclosure:</p>
+        <p className="Text">
+          We will not disclose any kind of personal data of our users to third
+          parties, including the providers of services for the Application.
+        </p>
+
+        <p className="Subtitle">Data Security:</p>
+        <p className="Text">
+          We take all necessary measures to ensure the security of our users’
+          personal data. This includes the implementation of technical and
+          physical measures to protect the information collected against any
+          unauthorized use, unauthorized access, disclosure, destruction, or
+          modification.
+        </p>
+
+      </div>
     </div>
   );
 }
